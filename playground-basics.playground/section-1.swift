a92cb1d1e1091cd9611b67e5568c8f40ba2066c4
Xiomara7 <figueroaxiomara7@gmail.com>
2015-02-25 22:28:56 -0400

    added helper comments and images

--- playground-basics.playground/section-1.swift
+++ playground-basics.playground/section-1.swift
@@ -1,33 +1,63 @@
 import UIKit
 
-var str = "Hello, playground"
+/*
+    //##### Explicits & Inferred Variable Values #####
 
-// Explicits & Inferred Values
+    var explicitVar : Int = 2
+    var inferredVar = 2
 
-let explicitVar : Int = 2
-let inferredVar = 2
+    explicitVar = 4
+    inferredVar = 4
+*/
 
-var colors = ["Blue", "Red", "Green"]
+/*
+    //##### Explicits & Inferred Constant Values #####
 
-// Closures
+    let explicitLet : Int = 2
+    let inferredLet = 2
 
-let names  = ["Chris", "Bobby", "Jom", "Julio", "Wendy"]
+    //explicitLet = 4
+    //inferredLet = 4
+*/ 
 
-func backwards(s1: String, s2: String) -> Bool {
-    return s1 > s2
-}
-var reversed = sorted(names, backwards)
 
-// Extensions
+/*
+    //##### Closures #####
 
-extension Int {
-    func repetitions(task: () -> ()) {
-        for i in 0..<self {
-            task()
+    let names  = ["Chris", "Bobby", "Jom", "Julio", "Wendy"]
+
+    func backwards(s1: String, s2: String) -> Bool {
+        return s1 > s2
+    }
+
+    var reversed = sorted(names, backwards)
+*/
+
+/*
+    //##### Extensions #####
+
+    extension Int {
+        func repetitions(task: () -> ()) {
+            for i in 0..<self {
+                task()
+            }
         }
     }
-}
 
-3.repetitions {
-    println("Xio")
-}
+    3.repetitions {
+        println("Xio")
+    }
+*/
+
+//var img1 = UIImage(named: "cat.jpeg")
+//let img2 = UIImage(named: "kitty.jpg")
+
+
+
+
+
+
+
+
+
+
